8f53cad389401376c82f6ea9dd5f299e08abefa1
yeonkyoung <keep.ykk@gmail.com>
2023-05-06 20:55:49 +0900

    Feat: 완주하지 못한 선수

---
+++ 프로그래머스/완주하지 못한 선수.py
@@ -0,0 +1,7 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    completion.append('')
+    for p,c in zip(participant,completion):
+        if p!=c:
+            return p
